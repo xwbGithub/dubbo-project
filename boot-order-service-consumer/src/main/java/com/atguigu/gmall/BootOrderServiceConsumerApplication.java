13e7e9d1847c4bea966a0c88bb6aa8d913816f1c
crown <1056481167@qq.com>
2018-12-03 00:25:54 +0800

    springboot-dubbo commit

---
+++ boot-order-service-consumer/src/main/java/com/atguigu/gmall/BootOrderServiceConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOrderServiceConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootOrderServiceConsumerApplication.class, args);
+    }
+}
---
+++ boot-order-service-consumer/src/main/java/com/atguigu/gmall/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.controller;
+
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping("/initOrder")
+    public List<UserAddress> initOrder(@RequestParam("uid") String userId) {
+        return orderService.initOrder(userId);
+    }
+}
